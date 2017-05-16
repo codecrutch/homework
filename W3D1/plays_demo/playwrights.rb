181be28042aa38b1ff63be0cc7bb8440ee585d18
Sean Snyder <sean@codecrutch.com>
2017-05-16 03:22:29 -0400

    SQL Homework

---
+++ W3D1/plays_demo/playwrights.rb
@@ -0,0 +1,55 @@
+require_relative 'plays'
+
+class Playwright
+  attr_accessor :name, :birth_year
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights WHERE name = ?", name)
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless @id
+    data = PlayDBConnection.instance.execute(<<-SQL, @name)
+      SELECT plays.*
+      FROM plays JOIN playwrights
+      ON plays.playwright_id = playwrights.id
+      WHERE name = ?
+    SQL
+
+    data.map { |datum| Play.new(datum) }
+  end
+end
