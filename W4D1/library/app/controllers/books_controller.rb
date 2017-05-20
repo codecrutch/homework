6ed08780a49d855d59acd6c9195fa155b7bd0dc1
Sean M. Snyder <sean.snyder@snhu.edu>
2017-05-20 12:11:16 -0400

    Finish homework part one

--- W4D1/library/app/controllers/books_controller.rb
+++ W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,24 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
   end
 
   def new
-    # your code here
+    @book = Book.new
   end
 
   def create
-    # your code here
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_url
+    end
   end
 
   def destroy
-    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
