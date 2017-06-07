e8709b73376ee7c5be0bf8b213783f59256da0b6
Sean M. Snyder <sean.snyder@snhu.edu>
2017-06-07 00:08:59 -0400

    Finish AJAX homework

--- W6D3/ajax-hw/js/index.js
+++ W6D3/ajax-hw/js/index.js
@@ -1,5 +1,18 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  type: 'POST',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+    console.log("We have your weather!");
+    console.log(data.weather[0].description);
+    console.log(data);
+  },
 
+  error() {
+    console.error("An error occured.");
+  },
+});
 // Add another console log here, outside your AJAX request
+console.log("AJAX Request Sent");
