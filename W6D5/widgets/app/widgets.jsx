86ff24a98aab6abd8e698fcf4e0c7b1d8ecc36bb
Eli Mordechai <emor0219@gmail.com>
2017-06-09 12:34:16 -0400

    Initial commit

---
+++ W6D5/widgets/app/widgets.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './components/clock/clock';
+
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById("main");
+	ReactDOM.render(<div />, root);
+});
