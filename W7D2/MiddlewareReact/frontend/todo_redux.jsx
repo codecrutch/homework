ace15f2300f57bace9c8efae17285539355b72d9
Sean M. Snyder <sean.snyder@snhu.edu>
2017-06-12 23:17:42 -0400

    Finish homework

---
+++ W7D2/MiddlewareReact/frontend/todo_redux.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+// const applyMiddlewares = function(store, middlewares) {
+//   let dispatch = store.dispatch;
+//
+//   middlewares.forEach( middleware => {
+//     dispatch = middleware(store)(dispatch);
+//   })
+//
+//   return Object.assign({}, store, { dispatch });
+// };
